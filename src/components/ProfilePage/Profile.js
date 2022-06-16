440e8a6564f663d76984e0be85e02df12603f3d3
limgabi <gabi520@naver.com>
2022-06-16 15:33:28 +0900

    update profile picture & display name

--- src/components/ProfilePage/Profile.js
+++ src/components/ProfilePage/Profile.js
@@ -1,14 +1,18 @@
 import React, { useRef, useState } from 'react';
-import { authService } from '../../fbase';
+import { authService, storageService } from '../../fbase';
 import { updateProfile } from 'firebase/auth';
+import { deleteObject, getDownloadURL, ref, uploadString } from 'firebase/storage';
 import Library from './Library';
 import style from "./Profile.module.css";
+import { HiPencil } from "react-icons/hi";
+import { MdOutlineClose } from "react-icons/md"
 
 function Profile({ userObj, refreshUser }) {
   const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const [clickUpdate, setClickUpdate] = useState(false);
   const [profileImg, setProfileImg] = useState(userObj.photoURL);
   const fileInput = useRef(null);
+  const basedURL = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
 
   const onFileChange = (e) => {
     const {
@@ -25,10 +29,10 @@ function Profile({ userObj, refreshUser }) {
     reader.readAsDataURL(theFile);
   }
 
-  const onClickUpdate = () => {
-    setClickUpdate(!clickUpdate);
+  const toggleUpdate = () => {
+    setClickUpdate((prev) => !prev);
   }
-  
+
   const onChange = (e) => {
     const {
       target: { value }
@@ -36,12 +40,40 @@ function Profile({ userObj, refreshUser }) {
     setNewDisplayName(value);
   }
 
-  const updateName = async (e) => {
+  const deleteProfile = async () => {
+    const fileRef = ref(storageService, `profileImg/${userObj.uid}`);
+    const ok = window.confirm("프로필 사진을 삭제하시겠습니까?");
+    if (ok) {
+      await deleteObject(fileRef);
+      setProfileImg(basedURL);
+    }
+    setClickUpdate(!clickUpdate)
+  }
+
+  const onSubmit = async (e) => {
     e.preventDefault();
-    if (userObj.displayName !== newDisplayName) {
+    if (userObj.displayName == newDisplayName) {
+      let profileImgURL = "";
+
+      if (profileImg !== "") {
+        const fileRef = ref(storageService, `profileImg/${userObj.uid}`);
+        const uploadFile = await uploadString(fileRef, profileImg, 'data_url');
+        console.log(uploadFile)
+        profileImgURL = await getDownloadURL(uploadFile.ref);
+        setProfileImg(profileImgURL);
+      }
+
+      if (userObj.displayName !== newDisplayName && userObj.photoURL !== profileImg) {
+        await updateProfile(authService.currentUser, { displayName: newDisplayName, photoURL: profileImgURL });
+        refreshUser();
+      } else if (userObj.photoURL !== profileImg) {
+        await updateProfile(authService.currentUser, { photoURL: profileImgURL });
+        refreshUser();
+      }
+    } else if (userObj.displayName !== newDisplayName) {
       await updateProfile(authService.currentUser, { displayName: newDisplayName });
       refreshUser();
-    }
+    } 
     setClickUpdate(!clickUpdate);
   }
 
@@ -49,38 +81,39 @@ function Profile({ userObj, refreshUser }) {
     <div className={style.container}>
       <div className={style.adminWrap}>
         <div className={style.adminInfo}>
-          <form>
+          <form onSubmit={onSubmit}>
             <div className={style.profile}>
-              <img
-                src={profileImg}
-                onClick={() => { fileInput.current.click() }}
-              />
+              {clickUpdate && <span onClick={deleteProfile} className={style.deleteImg}><MdOutlineClose /></span>}
+              <img src={profileImg} />
+              {clickUpdate && <span onClick={() => { fileInput.current.click() }}><HiPencil /></span>}
             </div>
-            <input
+            {<input
               type='file'
               style={{ display: 'none' }}
               accept='image/jpg,impge/png,image/jpeg'
               name='profile_img'
               onChange={onFileChange}
-              ref={fileInput} />
+              ref={fileInput} />}
             <div>
-              {clickUpdate 
+              {clickUpdate
                 ? (<input
                   type="text"
                   placeholder="닉네임을 입력하세요"
                   value={newDisplayName}
                   onChange={onChange}
-                  required
                   className={style.editInput}
                 />
                 ) : (<div className={style.name}>{userObj.displayName}</div>)
               }
             </div>
             <div className={style.btn}>
-              {
-                clickUpdate ? <button onClick={updateName}>update profile</button>
-                  : <button onClick={onClickUpdate}>edit profile</button>
-              }
+              {clickUpdate 
+                ? (
+                <div className={style.btnContainer}>
+                  <button onClick={toggleUpdate}>취소</button>
+                  <button onClick={onSubmit}>완료</button> 
+                </div>
+                ) : <button onClick={toggleUpdate}>프로필 편집</button>}
             </div>
           </form>
         </div>
